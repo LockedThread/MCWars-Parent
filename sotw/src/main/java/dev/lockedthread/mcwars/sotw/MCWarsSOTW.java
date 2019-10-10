f7f66715e7b4f2aceb72ac8c9fbc06f7cff8faca
LockedThread <proteinshakedev@gmail.com>
2019-10-10 17:57:55 -0400

    Initial code commit
    Took 5 minutes

---
+++ sotw/src/main/java/dev/lockedthread/mcwars/sotw/MCWarsSOTW.java
@@ -0,0 +1,16 @@
+package dev.lockedthread.mcwars.sotw;
+
+import com.gameservergroup.gsgcore.plugin.Module;
+
+public class MCWarsSOTW extends Module {
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void disable() {
+
+    }
+}
